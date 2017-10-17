edfe144da5d21c80098bae12d68a58c7b58d4312
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-10-17 15:42:28 +0100

    Updated patterns

--- funpdbe/funpdbe_accounts/urls.py
+++ funpdbe/funpdbe_accounts/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    url('register', views.register, name='register'),
-    url('home', views.home, name='home'),
-    url('login', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    url('logout', views.logout_view, name='logout')
+    url('register$', views.register, name='register'),
+    url('home$', views.home, name='home'),
+    url('login$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    url('logout$', views.logout_view, name='logout')
 ]
