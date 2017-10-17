fe3bd1bf93904983068a45a7daabf606e0e2a4d2
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-10-17 14:43:01 +0100

    Adding customised user form

---
+++ funpdbe/funpdbe_accounts/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserCreateForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    groups = Group
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "password1",
+            "password2",
+            "groups"
+
+        )
+
+    def save(self, commit=True):
+        user = super(UserCreateForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.is_active = False
+        if commit:
+            # Needs to save before group(s) can be added
+            user.save()
+            user.groups.set(self.cleaned_data["groups"])
+            user.save()
+
+        return user
