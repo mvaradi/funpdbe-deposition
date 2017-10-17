66fd842a2794fcfd6cc5c0764cf9fe9e04873a30
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-10-17 14:44:02 +0100

    Adding basic account views

--- funpdbe/funpdbe_accounts/views.py
+++ funpdbe/funpdbe_accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UserCreateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return HttpResponseRedirect('home')
+    else:
+        form = UserCreateForm()
+    return render(request, 'register.html', {'form': form})
+
+
+@login_required
+def home(request):
+    return render(request, 'home.html')
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('home')
