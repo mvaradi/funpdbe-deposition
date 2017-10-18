6334a64dfb567fe45189d39bd77651e0b0e02ace
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-10-18 14:52:34 +0100

    Refactoring tests to use reverse()

--- funpdbe_accounts/tests.py
+++ funpdbe_accounts/tests.py
@@ -1,46 +1,51 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class UrlTests(TestCase):
 
-    def test_if_login_url_is_valid(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/login')
-        self.assertEqual(response.status_code, 200)
+    def setUp(self):
+        self.client = Client()
 
-    def test_if_logout_url_is_valid(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/logout')
-        self.assertEqual(response.status_code, 302)
-
+    # Tests specific to register url
     def test_if_register_url_is_valid(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/register')
+        response = self.client.get(reverse("register"))
         self.assertEqual(response.status_code, 200)
 
-    def test_if_home_url_is_valid(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/home')
+    # Tests specific to login url
+    def test_if_login_url_is_valid(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    # Tests specific to logout url
+    def test_if_logout_url_is_valid(self):
+        response = self.client.get(reverse("logout"))
         self.assertEqual(response.status_code, 302)
 
     def test_if_logout_redirects_to_home(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/logout', follow=True)
+        response = self.client.get(reverse("logout"), follow=True)
         self.assertEqual(response.redirect_chain[0][0], 'home')
+
+    # Tests specific to home url
+    def test_if_home_url_is_valid(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 302)
 
 
 class ViewTests(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
+    # Tests specific to home view
     def test_if_home_redirects_if_not_logged_in(self):
-        c = Client()
-        response = c.get('/funpdbe_accounts/home')
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 302)
 
     def test_if_home_renders_when_logged_in(self):
         user = User.objects.create_user('test', 'test@test.test', 'test')
-        c = Client()
-        c.login(username='test', password='test')
-        response = c.get('/funpdbe_accounts/home')
+        self.client.login(username='test', password='test')
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
 
