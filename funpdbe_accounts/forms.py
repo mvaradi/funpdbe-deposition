359b805cbf63bd2e30197651a6c910febdbd990a
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-11-07 13:54:26 +0000

    Setting new accounts active by default

--- funpdbe_accounts/forms.py
+++ funpdbe_accounts/forms.py
@@ -25,7 +25,7 @@ class UserCreateForm(UserCreationForm):
         user.email = self.cleaned_data["email"]
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
-        user.is_active = False
+        user.is_active = True
         if commit:
             # Needs to save before group(s) can be added
             user.save()
