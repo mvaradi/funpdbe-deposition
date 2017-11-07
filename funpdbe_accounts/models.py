e70bd44a492bda44f284dc4942b1b958594cf75e
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-11-07 16:01:12 +0000

    Creating the FunPdbePartners model

--- funpdbe_accounts/models.py
+++ funpdbe_accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class FunPdbePartners(models.Model):
+
+    partner_name = models.CharField("Partner resource name", max_length=100, primary_key = True)
+    partner_owner = models.CharField("Owner of the resource", max_length=255)
+    partner_contact = models.EmailField("Contact email address", max_length=255)
+    work_package = models.IntegerField("Work package", null=True)
+    is_active_partner = models.BooleanField("Actively participating resource", default=True)
+
