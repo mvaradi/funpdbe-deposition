6c65cafabbfd3cc2dcce013714977f2d81711b86
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-11-08 12:13:01 +0000

    Updating the context of the home view with additional lists

--- funpdbe_accounts/views.py
+++ funpdbe_accounts/views.py
@@ -4,7 +4,8 @@ from .forms import UserCreateForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import Group
-from .models import FunPdbePartners
+from .models import RequestedPartner
+from .models import PartnerRequestedByUser
 
 def register(request):
     if request.method == "POST":
@@ -22,6 +23,15 @@ def home(request):
     """
     Home view of the account which lists all the partner resources
     a user contributes to
+
+    There are three distinct lists for specific purposes:
+    1.) the list of partner resources for which a given user has
+    submission and view permissions. This list is made from Group() objects
+    2.) the list of partner resources a given user would like to have permissions.
+    This list consists of RequestedPartner() objects
+    3.) another list with partner resources (RequestedPartner()) for which
+    the user already requested permissions, but it is pending action from
+    an administrator
     """
 
     """
@@ -35,27 +45,42 @@ def home(request):
         contributing_to.append(group.name)
 
     """
-    List of registered partner groups in FunPdbePartners() object. These 
+    List of resources associated with a user in PartnerRequestedByUser(),
+    but only those for which the corresponding Group() was not yet linked
+    
+    In other terms, this is a list of resources to which the user already
+    requested permissions, but has not yet been granted by the admin
+    """
+    pending_requests = []
+    requested = PartnerRequestedByUser.objects.select_related().all()
+    relevant_requests = requested.filter(user_ref__username__contains=request.user.username)
+    for relevant_request in relevant_requests:
+        pending_requests.append(relevant_request.partner_ref.partner_name)
+
+    """
+    List of registered partner groups in RequestedPartner() objects. These 
     objects have no permissions, and after manual curation (!) the corresponding
     Group() object should be created with the same name
     
     This is required so that users can suggests new partner resources, but they
     have to be investigated by an admin on a case to case basis
     """
-    not_contributing_to = []
-    for partner in FunPdbePartners.objects.all():
+    user_requests = []
+    for partner in RequestedPartner.objects.all():
         if partner.is_active_partner:
             if partner.partner_name not in contributing_to:
-                not_contributing_to.append(partner.partner_name)
+                if partner.partner_name not in pending_requests:
+                    user_requests.append(partner.partner_name)
 
-    account_details = {
+
+    context = {
         "user_name" : request.user.username,
         "user_groups" : contributing_to,
+        "user_pending": pending_requests,
+        "user_request": user_requests
     }
 
-
-
-    return render(request, 'home.html', {'account': account_details, "available_partners": not_contributing_to})
+    return render(request, 'home.html', context)
 
 
 def logout_view(request):
