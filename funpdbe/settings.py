6f3933f27339b58515acb84021075a587cefef53
Mihaly Varadi <mvaradi@ebi.ac.uk>
2017-12-11 16:07:07 +0000

    Adding funpdbe_data app to funpdbe project

--- funpdbe/settings.py
+++ funpdbe/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'funpdbe_accounts'
+    'funpdbe_accounts',
+    'funpdbe_data'
 ]
 
 MIDDLEWARE = [
