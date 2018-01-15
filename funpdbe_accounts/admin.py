c6f96d7d7febcda377e0b0c395c028d1f8209f9f
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-01-15 13:23:26 +0000

    Deleting account handling for now

--- funpdbe_accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import RequestedPartner
-from .models import PartnerRequestedByUser
-
-admin.site.register(PartnerRequestedByUser)
-admin.site.register(RequestedPartner)
--- funpdbe_accounts/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FunpdbeAccountsConfig(AppConfig):
-    name = 'funpdbe_accounts'
