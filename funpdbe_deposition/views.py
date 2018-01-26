675c197e2327c4efca309c47b950135f402579be
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-01-26 14:31:59 +0000

    Additional GET API views

--- funpdbe_deposition/views.py
+++ funpdbe_deposition/views.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from funpdbe_deposition.serializers import UserSerializer
 from funpdbe_deposition.models import Entry
+from funpdbe_deposition.models import RESOURCES
 from funpdbe_deposition.serializers import EntrySerializer
 from funpdbe_deposition.permissions import IsOwnerOrReadOnly
 
@@ -23,6 +24,7 @@ class EntryList(APIView):
         return Response(serializer.data)
 
     def post(self, request):
+        # TODO consider moving to single entry view
         # print(request.data)
         serializer = EntrySerializer(data=request.data)
         # print(serializer)
@@ -31,6 +33,43 @@ class EntryList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         # print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        # TODO
+        pass
+
+
+class EntryListByResource(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def get(self, request, resource):
+        for RESOURCE in RESOURCES:
+            if resource in RESOURCE:
+                entries = Entry.objects.filter(data_resource=resource)
+                serializer = EntrySerializer(entries, many=True)
+                return Response(serializer.data)
+        return Response("Invalid data resource", status=status.HTTP_400_BAD_REQUEST)
+
+
+class EntryDetail(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def get(self, request, pdb_id):
+        entries = Entry.objects.filter(pdb_id=pdb_id)
+        serializer = EntrySerializer(entries, many=True)
+        return Response(serializer.data)
+
+
+class EntryDetailByResource(APIView):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+    def get(self, request, resource, pdb_id):
+        for RESOURCE in RESOURCES:
+            if resource in RESOURCE:
+                entries = Entry.objects.filter(data_resource=resource).filter(pdb_id=pdb_id)
+                serializer = EntrySerializer(entries, many=True)
+                return Response(serializer.data)
+        return Response("Invalid data resource", status=status.HTTP_400_BAD_REQUEST)
 
 
 class UserList(generics.ListAPIView):
