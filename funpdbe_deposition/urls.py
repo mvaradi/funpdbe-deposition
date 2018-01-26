0050e9f4980294f43297d3d5283ec9270d1f634f
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-01-26 15:20:08 +0000

    Routes better separated

--- funpdbe_deposition/urls.py
+++ funpdbe_deposition/urls.py
@@ -3,9 +3,9 @@ from funpdbe_deposition import views
 
 urlpatterns = [
     url(r'^entries/$', views.EntryList.as_view()),
-    url(r'^entries/(?P<resource>[A-Za-z0-9]+)/$', views.EntryListByResource.as_view()),
-    url(r'^entries/(?P<resource>[A-Za-z0-9]+)/(?P<pdb_id>[A-Za-z0-9]{4})$', views.EntryDetailByResource.as_view()),
-    url(r'^entry/(?P<pdb_id>[A-Za-z0-9]{4})/$', views.EntryDetail.as_view()),
+    url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/$', views.EntryListByResource.as_view()),
+    url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/(?P<pdb_id>[A-Za-z0-9]{4})/$', views.EntryDetailByResource.as_view()),
+    url(r'^entries/pdb/(?P<pdb_id>[A-Za-z0-9]{4})/$', views.EntryDetail.as_view()),
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ]
