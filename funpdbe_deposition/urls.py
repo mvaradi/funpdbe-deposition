92a4f13fe763610fc383a02662097be7d02ea4a5
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-09 16:05:43 +0000

    Removing user views

--- funpdbe_deposition/urls.py
+++ funpdbe_deposition/urls.py
@@ -5,7 +5,5 @@ urlpatterns = [
     url(r'^entries/$', views.EntryList.as_view()),
     url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/$', views.EntryListByResource.as_view()),
     url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryDetailByResource.as_view()),
-    url(r'^entries/pdb/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+    url(r'^entries/pdb/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryListByPdb.as_view())
 ]
