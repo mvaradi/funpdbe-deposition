61b19ba6a3ce4c7538a138e349a95672480fc136
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-02-09 11:40:10 +0000

    Additional GET API tests

--- funpdbe_deposition/tests.py
+++ funpdbe_deposition/tests.py
@@ -4,9 +4,14 @@ from django.test import TestCase
 from django.test import Client
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
+from funpdbe_deposition.models import Entry
 
 
 class MockData(object):
+    """
+    This is a mock JSON data which complies with the JSON data schema
+    defined at https://github.com/funpdbe-consortium/funpdbe_schema
+    """
 
     def __init__(self):
         self.data = {
@@ -54,12 +59,130 @@ class MockData(object):
         }
 
 
-class ApiTests(TestCase):
+class ApiGetTests(TestCase):
+    """
+    Testing GET methods of the deposition API
+    """
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user("test", "test@test.test", "test")
+        self.entry = Entry.objects.create(owner_id=1, pdb_id="0x00", data_resource="funsites")
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Entry.objects.all().delete()
+
+    """
+    Test simple GET for all entries
+    This should return 200
+    """
+    def test_getting_all_entries(self):
+        response = self.client.get("/funpdbe_deposition/entries/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    Test GET all entries when none exists
+    This should return 404
+    """
+    def test_getting_all_entries_when_none(self):
+        Entry.objects.all().delete()
+        response = self.client.get("/funpdbe_deposition/entries/")
+        self.assertEqual(response.status_code, 404)
+        self.entry = Entry.objects.create(owner_id=1, pdb_id="0x00", data_resource="funsites")
+
+    """
+    Test GET with PDB id
+    This should return 200
+    """
+    def test_getting_one_by_pdb_id(self):
+        response = self.client.get("/funpdbe_deposition/entries/pdb/0x00/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    Test GET with PDB id which does not exist
+    This should return 404
+    """
+    def test_getting_one_by_pdb_id_non(self):
+        Entry.objects.all().delete()
+        response = self.client.get("/funpdbe_deposition/entries/pdb/0x00/")
+        self.assertEqual(response.status_code, 404)
+        self.entry = Entry.objects.create(owner_id=1, pdb_id="0x00", data_resource="funsites")
+
+    """
+    Test GET with invalid PDB id
+    This should return 400 (bad request)
+    """
+    def test_getting_one_by_invalid_pdb_id(self):
+        response = self.client.get("/funpdbe_deposition/entries/pdb/invalid/")
+        self.assertEqual(response.status_code, 400)
+
+    """
+    Test GET all by resource
+    This should return 200
+    """
+    def test_getting_all_by_resource(self):
+        response = self.client.get("/funpdbe_deposition/entries/resource/funsites/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    Test GET all by resource when non entry exists
+    This should return 404
+    """
+    def test_getting_all_by_resource_none(self):
+        Entry.objects.all().delete()
+        response = self.client.get("/funpdbe_deposition/entries/resource/funsites/")
+        self.assertEqual(response.status_code, 404)
+        self.entry = Entry.objects.create(owner_id=1, pdb_id="0x00", data_resource="funsites")
+
+    """
+    Test GET with PDB id and resource
+    This should return 200
+    """
+    def test_getting_one_by_pdb_id_and_resource(self):
+        response = self.client.get("/funpdbe_deposition/entries/resource/funsites/0x00/")
+        self.assertEqual(response.status_code, 200)
+
+    """
+    Test GET with PDB id and resource when entry is not there
+    This should return 404
+    """
+    def test_getting_one_by_pdb_id_and_resource_none(self):
+        Entry.objects.all().delete()
+        response = self.client.get("/funpdbe_deposition/entries/resource/funsites/0x00/")
+        self.assertEqual(response.status_code, 404)
+        self.entry = Entry.objects.create(owner_id=1, pdb_id="0x00", data_resource="funsites")
+
+    """
+    Test GET all by invalid resource
+    This should return 400 (bad request)
+    """
+    def test_getting_all_by_invalid_resource(self):
+        response = self.client.get("/funpdbe_deposition/entries/resource/whatever/")
+        self.assertEqual(response.status_code, 400)
+
+    """
+    Test GET with PDB id and invalid resource
+    This should return 400 (bad request)
+    """
+    def test_getting_one_by_pdb_id_and_bad_resource(self):
+        response = self.client.get("/funpdbe_deposition/entries/resource/whatever/0x00/")
+        self.assertEqual(response.status_code, 400)
+
+    """
+    Test GET with invalid PDB id and resource
+    This should return 400 (bad request)
+    """
+    def test_getting_one_by_bad_pdb_id_and_resource(self):
+        response = self.client.get("/funpdbe_deposition/entries/resource/funsites/whatever/")
+        self.assertEqual(response.status_code, 400)
+
+
+class ApiPostTests(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.data = MockData()
-
 
     """
     Test if POST is allowed without providing user login information
@@ -69,15 +192,12 @@ class ApiTests(TestCase):
         response = self.client.post("/funpdbe_deposition/entries/resource/funsites/", data=self.data.data)
         self.assertEqual(response.status_code, 403)
 
-
     """
     Test if POST is allowed when user does provide login information,
     but does not belong to the group of the resource the user tries 
     to POST to
-    
     For example user "foo" wants to POST to resource "bar", but does not
     have such permission
-    
     This should fail with 403 (forbidden)
     """
     def test_posting_without_permission(self):
@@ -88,11 +208,9 @@ class ApiTests(TestCase):
         self.client.logout()
         user.delete()
 
-
     """
     Test if POST works for a user who has permission to POST to a
     specific resource
-    
     This should succeed with 201 (created)
     """
     def test_posting_with_permission(self):
@@ -106,11 +224,9 @@ class ApiTests(TestCase):
         user.delete()
         group.delete()
 
-
     """
     Test if POST works for a permitted user who sends data where the resource
     name does not match the intended resource name 
-    
     This should fail with 400 (bad request)
     """
     def test_posting_with_permission_but_resource_mismatch(self):
@@ -124,13 +240,12 @@ class ApiTests(TestCase):
         user.delete()
         group.delete()
 
+
     """
     Test if POST works for a permitted user who sends data that is
     already in the database
-
     This should fail with 400 (bad request)
     """
-
     def test_posting_with_permission_but_entry_is_there_already(self):
         group = Group.objects.create(name="funsites")
         user = User.objects.create_user('test', 'test@test.test', 'test')
@@ -145,10 +260,17 @@ class ApiTests(TestCase):
 
 
     """
-    Test simple GET for all entries
-    
-    This should return 200
+    Test if POST works for a user who provides invalid
+    resource name
+    This should fail with 400 (bad request)
     """
-    def test_getting_all_entries(self):
-        response = self.client.get("/funpdbe_deposition/entries/")
-        self.assertEqual(response.status_code, 200)
+    def test_posting_with_invalid_resource_name(self):
+        group = Group.objects.create(name="foo")
+        user = User.objects.create_user('test', 'test@test.test', 'test')
+        group.user_set.add(user)
+        self.client.login(username='test', password='test')
+        response = self.client.post("/funpdbe_deposition/entries/resource/foo/", data=self.data.data)
+        self.assertEqual(response.status_code, 400)
+        self.client.logout()
+        user.delete()
+        group.delete()
