7d39ed0815c35ed9f52c47a6d9a449d6220ba9f7
Mihaly Varadi <mvaradi@ebi.ac.uk>
2018-03-27 14:24:11 +0100

    Changing URL pattern (extending)

--- funpdbe_deposition/urls.py
+++ funpdbe_deposition/urls.py
@@ -3,7 +3,7 @@ from funpdbe_deposition import views
 
 urlpatterns = [
     url(r'^entries/$', views.EntryList.as_view()),
-    url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/$', views.EntryListByResource.as_view()),
-    url(r'^entries/resource/(?P<resource>[A-Za-z0-9]+)/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryDetailByResource.as_view()),
+    url(r'^entries/resource/(?P<resource>[A-Za-z0-9\-]+)/$', views.EntryListByResource.as_view()),
+    url(r'^entries/resource/(?P<resource>[A-Za-z0-9\-]+)/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryDetailByResource.as_view()),
     url(r'^entries/pdb/(?P<pdb_id>[A-Za-z0-9]+)/$', views.EntryListByPdb.as_view())
 ]
